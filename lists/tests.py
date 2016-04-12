ffe0dae59a44547c8311db3d1b9e19baf05cc94f
Andrew McCarthy <me@andrewmccarthy.ie>
2016-04-12 10:22:59 +0100

    Changes to Chapter 3 to use py.test
    - Rename the lists/tests.py test class to start with "Test"
    
    - Change unittest assertions to normal assert statements
    
    - Add the "live_server" pytest-django fixture, which means you don't
      need to run "manage.py runserver" before running tests

--- lists/tests.py
+++ lists/tests.py
@@ -1,20 +1,19 @@
 from django.core.urlresolvers import resolve
-from django.test import TestCase
 from django.http import HttpRequest
 
 from lists.views import home_page
 
 
-class HomePageTest(TestCase):
+class TestHomePage:
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
-        self.assertEqual(found.func, home_page)
+        assert found.func == home_page
 
 
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+        assert response.content.startswith(b'<html>')
+        assert b'<title>To-Do lists</title>' in response.content
+        assert response.content.endswith(b'</html>')
