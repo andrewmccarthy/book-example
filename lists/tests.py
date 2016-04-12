20519dced302b3225a73d07c2fccb02ab20e853e
Andrew McCarthy <me@andrewmccarthy.ie>
2016-04-12 10:43:09 +0100

    Changes to Chapter 4 to use py.test
    - Again, simplify assertions
    
    - Add comment about render_to_string problem with Django 1.9

--- lists/tests.py
+++ lists/tests.py
@@ -5,15 +5,18 @@ from django.test import TestCase
 
 from lists.views import home_page
 
-class HomePageTest(TestCase):
+class TestHomePage:
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
-        self.assertEqual(found.func, home_page)
+        assert found.func == home_page
 
 
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
+        # If using Django 1.9 add request=request to fix a new problem
+        # https://groups.google.com/forum/#!topic/obey-the-testing-goat-book/fwY7ifEWKMU
+        # expected_html = render_to_string('home.html', request=request)
         expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        assert response.content.decode() == expected_html
